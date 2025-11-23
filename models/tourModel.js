fd7a99048f1fd5857372147b1c246c51f9847d59
Uzair <uzairmaqbool050@gmail.com>
2025-11-23 16:26:31 +0500

    Add virtual property of durationInWeeks and document middleware to add slug property

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,51 +1,74 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, 'Tour must contain a name'],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'Tour must contain duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour must contain group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour must contain difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      required: [true, 'Tour must contain a Summary'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Tour must contain description'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour must contain a Price'],
+    },
+    imageCover: String,
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [String],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: [true, 'Tour must contain a name'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Tour must contain duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour must contain group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Tour must contain difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  summary: {
-    type: String,
-    required: [true, 'Tour must contain a Summary'],
-  },
-  description: {
-    type: String,
-    required: [true, 'Tour must contain description'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must contain a Price'],
-  },
-  imageCover: String,
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [String],
+tourSchema.virtual('durationInWeeks').get(function () {
+  return this.duration / 7;
 });
+
+// Document Middleware
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  // console.log(this);
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+// });
 
 module.exports = Tour = mongoose.model('Tour', tourSchema);
