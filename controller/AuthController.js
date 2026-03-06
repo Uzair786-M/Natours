3767abaaad26482c9f4ca080bb256b21cb391384
Uzair <uzairmaqbool050@gmail.com>
2026-03-06 19:22:19 +0500

    feat: send jwt token via cookie refactor:add reuseable function for jwt token creation and sending

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
-const signToken = require('../Utils/JWTToken').signToken;
+
+const { signToken } = require('../Utils/JWTToken');
 const APIError = require('./../Utils/apiErrors');
 const jwt = require('jsonwebtoken');
 const sendEmail = require('../Utils/email');
@@ -11,10 +12,25 @@ const asyncCatch = (fn) => {
   };
 };
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    data: 'Users are not defined yet',
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
   });
 };
 
@@ -27,34 +43,8 @@ exports.createUsers = asyncCatch(async (req, res, next) => {
     role: req.body.role,
   });
 
-  const token = signToken(newUser._id);
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: newUser,
-  });
+  createSendToken(newUser, 201, res);
 });
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    data: 'Users are not defined yet',
-  });
-};
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    data: 'Users are not defined yet',
-  });
-};
-
-exports.deleteUser = async (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    data: 'Users are not defined yet',
-  });
-};
 
 exports.protect = asyncCatch(async (req, res, next) => {
   let token;
@@ -67,7 +57,7 @@ exports.protect = asyncCatch(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   }
 
-  // 2) If no token exists,sending error in responce
+  // 2) If no token exists,sending error in response
   if (!token) {
     return next(new APIError('Please login to get access', 401));
   }
@@ -167,9 +157,24 @@ exports.resetPassword = asyncCatch(async (req, res, next) => {
   // 3) update passwordChangedAt property
 
   // 4) Send JWT to for login user
-  const token = signToken(user._id);
-  res.status(201).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 201, res);
+});
+
+exports.updateMyPassword = asyncCatch(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new APIError('Your current password is wrong.', 401));
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended!
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 201, res);
 });
