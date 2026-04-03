39a3fad1608cafd1c34c0ba7cebbd3f5453b5b60
Uzair <uzairmaqbool050@gmail.com>
2026-04-03 16:48:56 +0500

    feat:Add cards markup and styles for overview page

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,7 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+
+exports.getOverview = async (req, res) => {
+  //1) Get all tours from database
+  const tours = await Tour.find();
+  //2) Build template for overview page
+
+  //3) Render tempelates to browser
+
   res.status(200).render('overview', {
     title: 'All tours',
-    overView: 'This is overview page',
+    tours,
   });
 };
 
